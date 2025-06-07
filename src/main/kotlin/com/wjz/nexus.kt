228e45c036a0d3f693d73b3d9958fa1043f18d90
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-06-07 16:48:23 +0800

    用fabric template初始化

--- src/main/kotlin/com/wjz/nexus.kt
+++ src/main/kotlin/com/wjz/nexus.kt
@@ -1,9 +1,13 @@
 package com.wjz
 
 import net.fabricmc.api.ModInitializer
-
-class nexus : ModInitializer {
-
-    override fun onInitialize() {
-    }
+import org.apache.logging.log4j.LogManager
+object Nexus : ModInitializer {
+    private val logger = LogManager.getLogger("wjz-nexus")
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
 }
