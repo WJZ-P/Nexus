0e20b919315905cfcb6a95f59f10ade59742fa50
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-06-09 00:45:30 +0800

    wow!今天学了好多组件，真不错！我能用它们干点啥呢？

--- src/main/kotlin/com/wjz/Keybindings.kt
+++ src/main/kotlin/com/wjz/Keybindings.kt
@@ -1,5 +1,6 @@
 package com.wjz
 
+import com.wjz.guiExamples.ExampleGui
 import gg.essential.universal.UScreen
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
@@ -23,7 +24,7 @@ object KeyBindings {
 
         // 注册按键事件
         ClientTickEvents.END_CLIENT_TICK.register { client ->
-            while (openGuiKey.wasPressed()) {
+            if (openGuiKey.wasPressed()) {
                 // 打开GUI
                 UScreen.displayScreen(ExampleGui())
             }
---
+++ src/main/kotlin/com/wjz/guiExamples/ComponentsGui.kt
@@ -0,0 +1,4 @@
+package com.wjz.guiExamples
+
+class ComponentsGui {
+}
